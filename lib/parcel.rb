1dabb78ee5ef71b664fb6571a8e3f8cf28c4e378
Jeanette Fairless & Jocelyn Alsdorf <student@epicodus.com>
2015-04-27 16:45:55 -0700

    initialize files fro basic rspec testing and creates class of Parcel and method of volume

---
+++ lib/parcel.rb
@@ -0,0 +1,13 @@
+class Parcel
+  define_method(:initialize) do |side1, side2, side3, side4, weight|
+    @side1 = side1
+    @side2 = side2
+    @side3 = side3
+    @side4 = side4
+    @weight = weight
+  end
+
+  define_method(:volume) do
+    volume = @side1 + @side2 + @side3 + @side4
+  end
+end
