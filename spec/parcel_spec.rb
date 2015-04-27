e48928c801ef2199637deaf9cbc6b39997784d8a
Jeanette Fairless & Jocelyn Alsdorf <student@epicodus.com>
2015-04-27 16:50:04 -0700

    it calculates volume correctly

--- spec/parcel_spec.rb
+++ spec/parcel_spec.rb
@@ -1,2 +1,12 @@
 require('rspec')
 require('parcel')
+
+
+describe(Parcel) do
+  describe('#volume') do
+    it('returns sum of all sides of parcel') do
+    test_parcel = Parcel.new(3,3,4,5, 20)
+    expect(test_parcel.volume()).to(eq(15))
+    end
+  end
+end
